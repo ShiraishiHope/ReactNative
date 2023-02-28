79688b7d33296f3c32350489d021bce55d522265
RyanM <ryan.mazri@yahoo.fr>
2023-02-28 15:29:57 +0100

    v0.5.0: Added modalS with edition of data upon selecting elements

--- TodoList/App.js
+++ TodoList/App.js
@@ -1,5 +1,16 @@
-import {useState} from 'react';
-import {StyleSheet, Text, View, FlatList, TextInput, Button, StatusBar} from 'react-native';
+import React, {useState} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    FlatList,
+    Pressable,
+    Modal,
+    KeyboardAvoidingView,
+    Platform,
+    TouchableWithoutFeedback,
+    Keyboard, TextInput
+} from 'react-native';
 import DeleteButton from './components/DeleteButton';
 
 const sampleGoals = [
@@ -16,53 +27,102 @@ const sampleGoals = [
 ];
 
 export default function App() {
+
     const [objective, setObjectives] = useState('')
     const [objectivesList, setObjectivesList] = useState(sampleGoals)
+    const [selectedObjective, setSelectedObjective] = useState('');
+    const [modalVisible, setModalVisible] = useState(false)
     const handleDelete = (index) => {
         const newObjectivesList = [...objectivesList];
         newObjectivesList.splice(index, 1);
         setObjectivesList(newObjectivesList);
     }
     const renderItem = ({index, item}) => (
-        <View style={{flexDirection: 'row',justifyContent:'space-between'}}>
-            <Text style={styles.objective}>{item} </Text>
-            <DeleteButton
-                style={styles.deleteButton}
-                title="Delete" onPress={() => handleDelete(index)}
-                onPress={() => handleDelete(index)}
-            />
-        </View>)
-    const handleButtonPress = () => {
-        setObjectivesList([...objectivesList, objective]);
-        setObjectives('');
-    }
-    return (
+        <>
+        <Pressable
+            style={{flexDirection: 'row', justifyContent: 'space-between'}}
+            onPress={() => {
+                setSelectedObjective(item)
+                setModalVisible(true)
 
-        <View style={styles.container}>
-
-
-            <Text style={styles.text}>What's 42?</Text>
-
-                <FlatList
-                    contentContainerStyle={styles.containerList}
-                    data={objectivesList}
-                    renderItem={renderItem}
-                    keyExtractor={(item, index) => index.toString()}
-                />
-                <View style={styles.inputView}>
+            }}>
+            <View style={styles.objectiveCard}>
+                <Text style={[styles.objective, item === selectedObjective ? styles.completedObjective : null]}>
+                    {item}
+                </Text>
+            </View>
+        </Pressable>
+    <Modal
+        animationType={'slide'}
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+            setModalVisible(!modalVisible)
+        }}>
+        <View style={styles.centeredView}>
+            <View style={styles.modalView}>
                 <TextInput
-                    style={styles.input}
-                    placeholder={"Don't write here"}
-                    value={objective}
-                    onChangeText={(text) => setObjectives(text)}
+                    style={styles.textStyle}
+                    value={selectedObjective}
+                    onChangeText={(text) => setSelectedObjective(text)}
                 />
-                <Button
-                    title="Don't Press me"
-                    onPress={handleButtonPress}
-                />
+                <View style={{ flexDirection: 'row' }}>
+                    <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            setModalVisible(!modalVisible);
+                            setSelectedObjective('');
+                        }}>
+                        <Text style={styles.textStyle}>Cancel</Text>
+                    </Pressable>
+                    <Pressable
+                        style={[styles.button, styles.buttonSave]}
+                        onPress={() => {
+                            editButtonPress(selectedObjective,index);
+                            setModalVisible(!modalVisible);
+                            setSelectedObjective('');
+                        }}>
+                        <Text style={styles.textStyle}>Save</Text>
+                    </Pressable>
                 </View>
+            </View>
+        </View>
+    </Modal>
+        </>
+    )
+    /*            <DeleteButton
+                    title="Delete" onPress={() => handleDelete(index)}
+                    onPress={() => handleDelete(index)}
+                />*/
+    const addButtonPress = (item) => {
+        const newObjectivesList = [...objectivesList]
+        const index = newObjectivesList.indexOf(item)
+        if (selectedObjective === '') {
+            newObjectivesList.push(objective)
+        } else if (index !== -1) {
+            newObjectivesList[index] = selectedObjective
+        }
+        setObjectivesList(newObjectivesList)
+        setModalVisible(false)
+        setSelectedObjective('')
+    }
+    const editButtonPress = (item, index) => {
+        const updateList = objectivesList.map((currentItem, currentIndex) => (
+            index == currentIndex ? item : currentItem
+        ))
+        setObjectivesList(updateList)
+    }
 
-
+    /*START OF RETURN*/
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>To-do List</Text>
+            <FlatList
+                contentContainerStyle={styles.containerList}
+                data={objectivesList}
+                renderItem={renderItem}
+                keyExtractor={(item, index) => index.toString()}
+            />
 
         </View>
     );
@@ -79,26 +139,79 @@ const styles = StyleSheet.create({
     text: {
         color: '#F00',
         fontWeight: 'bold',
-        marginTop : 50
+        marginTop: 50,
+        fontSize: 20,
     },
-    input: {
-        height: 40,
-        minWidth: 80,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-    },
-    objective: {
-        alignSelf: 'center',
-        marginRight : 10
-    },
-    containerList: {
-        flex: 1,
-        justifyContent: 'center',
-    },
+
 
     inputView: {
         alignSelf: 'center',
         flexDirection: 'row'
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    keyboardAvoidingContainer: {
+        flex: 1,
+    },
+    input: {
+        flex: 1,
+        marginRight: 10,
+        paddingBottom: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+    },
+    objective: {
+        alignSelf: 'center',
+    },
+    completedObjective: {
+        textDecorationLine: 'line-through',
+    },
+    objectiveCard: {
+        backgroundColor: '#fff',
+        padding: 10,
+        marginVertical: 5,
+        marginHorizontal: 10,
+        borderRadius: 10,
+        elevation: 2,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+        margin: 5,
+        minWidth: 80,
+        alignItems: 'center',
+    },
+    buttonClose: {
+        backgroundColor: '#2196F3',
+    },
+    buttonSave: {
+        backgroundColor: '#4CAF50',
     },
 });
