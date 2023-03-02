a2cff18a3dfdd409be4c168b4c86ba67ad39d7dd
RyanM <ryan.mazri@yahoo.fr>
2023-03-02 23:16:20 +0100

    V1.0.0: Added geolocalisation and temperature.

--- Meteo/App.js
+++ Meteo/App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, StyleSheet, Text, View, Image, ScrollView} from 'react-native';
+import Constants from "expo-constants";
+import * as Location from "expo-location";
+import axios from "axios";
+import CurrentWeather from "./components/CurrentWeather";
+const apiUrl = (lat, lon) => `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=f21195c07b10c55ee15a8f363c1b3713&units=metric`;
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [loading, setLoading] = useState(true)
+    const [data, setData] = useState(null)
+
+
+    useEffect(() => {
+        const getCoordinates = async () => {
+            const {status} = await Location.requestForegroundPermissionsAsync()
+            if (status !== "granted") {
+                return
+            }
+            const userLocation = await Location.getCurrentPositionAsync()
+            getWeather(userLocation)
+        }
+        getCoordinates()
+    }, [])
+
+    const getWeather = async (location) => {
+        try {
+            const response = await axios.get(apiUrl(location.coords.latitude, location.coords.longitude))
+            setData(response.data)
+            setLoading(false)
+        } catch (error) {
+            console.log("getWeather crashed. You did something wrong")
+        }
+    }
+
+    if (loading) {
+        return <View>
+            <ActivityIndicator/>
+        </View>
+    }
+    return (
+        <View style={styles.container}>
+            <CurrentWeather data = {data}/>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#6A7',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
